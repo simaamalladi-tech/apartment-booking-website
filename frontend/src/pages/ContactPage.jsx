b534cba49d616a6d10778240e6d9c5b444213ffb
Your Name <your-email@example.com>
2026-03-10 13:31:48 +0100

    Update map to Gustav-Adolf-Straße 146A, 13086 Berlin

--- frontend/src/pages/ContactPage.jsx
+++ frontend/src/pages/ContactPage.jsx
@@ -166,7 +166,7 @@ function ContactPage({ onBookNow }) {
           <div className="map-wrapper">
             <iframe
               title="Alt-Berliner Eckkneipe Location"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2425.8!2d13.4574!3d52.5492!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNTLCsDMyJzU3LjEiTiAxM8KwMjcnMjYuNiJF!5e0!3m2!1sde!2sde!4v1"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2424.5!2d13.4342755!3d52.5547119!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84df1937ffd3f%3A0x149023e694b8d638!2sGustav-Adolf-Stra%C3%9Fe%20146A%2C%2013086%20Berlin!5e0!3m2!1sde!2sde!4v1"
               width="100%"
               height="400"
               style={{ border: 0 }}
